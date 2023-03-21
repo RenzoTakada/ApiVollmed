04a7586eefae00d4b1511e1b141ce555ac0ae69c
Renzo <renzotakada1@gmail.com>
2023-03-21 15:57:04 -0300

    Feat: criando tratamento de errors

--- src/main/java/med/ApiMed/Infra/TratamentoDeErros.java
+++ src/main/java/med/ApiMed/Infra/TratamentoDeErros.java
@@ -2,14 +2,28 @@ package med.ApiMed.Infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratamentoDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity erro404(){
+    public ResponseEntity erro404() {
         return ResponseEntity.notFound().build();
     }
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(errorValidacao::new).toList());
+    }
+    private record errorValidacao(String campo,String mensagem) {
+        public errorValidacao(FieldError erros) {
+            this(erros.getField(), erros.getDefaultMessage());
+        }
+    }
 }
+
+
+
